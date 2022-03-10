4d56530964a7e9d1de190c605d6125632e396b48
NourNafea <63984872+NourNafea@users.noreply.github.com>
2022-03-10 11:08:10 +0200

    Update Program.cs

--- DependencyInjection/Program.cs
+++ DependencyInjection/Program.cs
@@ -1,5 +1,5 @@
 ﻿using DependencyInjection;
 
 ApplicationDbContext applicationDbContext = new ApplicationDbContext();
-user user1 = new user(applicationDbContext);
-user1.show();
+user user = new user(applicationDbContext);
+user.show();
